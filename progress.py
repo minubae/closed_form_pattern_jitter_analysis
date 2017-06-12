9bb02e9064dbaf64bd3c9bf8f82d1ba3ad9c4b6a
Minu Bae <minubae.nyc@gmail.com>
2017-06-12 14:48:53 -0400

    modified progress.py

--- progress.py
+++ progress.py
@@ -3,12 +3,12 @@ import numpy as np
 # Observed Spike Train
 # x = np.random.uniform(0,1,(6,6))
 # x = np.random.uniform(0,1,(6,6))
-x = np.random.randint(2, size=10)
-size = len(x)
+ori_x = np.random.randint(2, size=10)
+size = len(ori_x)
 L = 2
 
 for i in range(0, size, L):
     print(i)
     #print(x[i])
 
-print(x)
+print(ori_x)
