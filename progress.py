9f1a7dd05325b2c77dcdff5cbe5bf11d8e48fd92
Minu Bae <minubae.nyc@gmail.com>
2017-06-13 13:29:56 -0400

    modified progress.py

--- progress.py
+++ progress.py
@@ -2,13 +2,30 @@ import numpy as np
 
 # Observed Spike Train
 # x = np.random.uniform(0,1,(6,6))
-# x = np.random.uniform(0,1,(6,6))
-ori_x = np.random.randint(2, size=10)
-size = len(ori_x)
-L = 2
+# Generating a binary random spike train with size = n
+obs_x = np.random.randint(2, size=20)
+size = len(obs_x)
+L = 4
 
-for i in range(0, size, L):
-    print(i)
-    #print(x[i])
+# Loop iteration with L-increments
+#for i in range(0, size, L): #print(x[i])
 
-print(ori_x)
+# The Observed spike train, a non-decreasing sequence of spike times
+x = []
+for i in range(size):
+    if obs_x[i] == 1:
+        x.append(i+1)
+
+sqs_spike_time_x = np.array(x)
+
+# Jitter Window
+Omega = []
+n = len(sqs_spike_time_x)
+for i in range(n):
+    for j in range(L):
+        Omega.append(sqs_spike_time_x[i] - np.floor(L/2) + j)
+
+
+print(obs_x)
+print(sqs_spike_time_x)
+print(Omega)
