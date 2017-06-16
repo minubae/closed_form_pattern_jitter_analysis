90a327cb9197528e97e08e9c458717b13ac80e33
minwoobae <minubae.nyc@gmail.com>
2017-06-16 15:37:48 -0400

    updated a function that generates Gamma[i]

--- progress.py
+++ progress.py
@@ -55,27 +55,26 @@ Omega = np.array(y).reshape(n, L)
 # {R+1, R+2,...}  if x_tilde_{i} - x_tilde_{i-1} is greater than R.
 # The parameter R controls the amount of history that is preserved. Larger values of R
 # enforce more regularity in the firing patterns across the resampled spike trains.
-R = 3
-gamma = []
-gamma.append(0)
+R = 2
+Gamma = []
+Gamma.append(0)
 for i in range(1, n):
-    #print(x_tilde[i])
     if x_tilde[i] - x_tilde[i-1] <= R:
-        print('hello 01')
-        gamma.append(x_tilde[i] - x_tilde[i-1])
+        Gamma.append(x_tilde[i] - x_tilde[i-1])
     else:
-        x = np.arange(R,L,1)
-        #gamma.append(x)
-        #print('hello 02', gamma)
-        print('hello 02')
-        #break
+        x = np.arange(R+1,R+1+L,1)
+        Gamma.append(x)
 
-print('gamma:', gamma)
 #print('Hello World!!')
 print("Observed_X: ", obs_x)
 print("x_tilde: ", x_tilde)
 print("Omega: ")
 print(Omega)
+print('Gamma:')
+print(Gamma)
+
+# for i in Gamma:
+#     print('Gamma:', i)
 
 # Iterate over Omega matrix columnwise
 #for i in Omega:
