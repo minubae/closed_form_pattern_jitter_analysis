17f88c77dc433f49da6ec6c32b8bb05c6c3c2a1a
minwoobae <minubae.nyc@gmail.com>
2017-06-27 11:50:30 -0400

    created main.py to control the entire process

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+# class Example(object):
+#     def run(self):
+#         print("Hello, world!")
+
+
+from Example import Example
+# import progress.get_x_tilde
+
+def main():
+
+    # print('Hello Main')
+    ex = Example()
+    ex.run()
+
+    # obs_x = np.random.randint(2, size=20)
+    # print(get_x_tilde(obs_x))
+
+if __name__ == "__main__":
+    main()
