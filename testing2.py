fc53ffc2f64a9ca758c3b6e7eb085917b9d0d459
minwoobae <minubae.nyc@gmail.com>
2017-07-19 18:37:51 -0400

    created a function: getSurrogate(tDistMatrix)

--- testing2.py
+++ testing2.py
@@ -3,19 +3,64 @@ import numpy as np
 # x = np.random.choice(np.arange(1, 7), p=[0.1, 0.05, 0.05, 0.2, 0.4, 0.2])
 # print('x: ', x)
 
-tDist = [0.2, 0.1, 0.15, 0.15, 0.25, 0.15]
-def roll(distribution):
-    print('Transition Dist.:', distribution)
-    randRoll = np.random.random() # in [0,1)
-    print('Random Roll:', randRoll)
+tDist = [0.2, 0.2, 0.15, 0.20, 0.25]
+def roll(dist):
+
+    randX = np.random.random() # in [0,1)
+
+    print('Transition Dist.:', dist)
+    print('Random X:', randX)
+
     sum = 0
     result = 1
-    for k in distribution:
-        print('Dist:', k)
+    i = 1
+    for k in dist:
+        print('P[',i,']:', k)
         sum += k
-        if randRoll <= sum:
+        if randX <= sum:
             print('Sum:', sum)
             return result
+        i += 1
         result+=1
 
-print('X:', roll(tDist))
+# print('X:', roll(tDist))
+
+
+
+tDistMat = np.matrix([[0.20, 0.20, 0.15, 0.20, 0.25],
+                      [0.15, 0.25, 0.10, 0.30, 0.20],
+                      [0.25, 0.15, 0.20, 0.20, 0.20],
+                      [0.10, 0.30, 0.20, 0.10, 0.30],
+                      [0.30, 0.20, 0.15, 0.20, 0.15]])
+# print('Transition Dist Matrix: ')
+# print(tDistMat)
+def getSurrogate(tDistMatrix):
+
+    sampleX = []
+    n = len(tDistMatrix)
+
+    for i, row in enumerate(tDistMat):
+        print('tDistMat[', i,']:', row)
+        rowDist = np.squeeze(np.array(row))
+        sum = 0
+        result = 1
+        randX = np.random.random()
+        m = len(rowDist)
+        print('Rand_X:', randX)
+        # print(rowDist)
+
+        for j in range(m):
+            print(j, rowDist[j])
+            sum += rowDist[j]
+
+            if randX <= sum:
+                print('Sum:', sum)
+                print('result: ', result, '\n')
+                sampleX.append(result)
+                break
+
+            result+=1
+    return sampleX
+
+
+print('X_1: ', getSurrogate(tDistMat))
