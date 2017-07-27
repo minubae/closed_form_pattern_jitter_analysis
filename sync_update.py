97708e529c49411b509649b4afaed128d8caf06e
minwoobae <minubae.nyc@gmail.com>
2017-07-27 15:30:35 -0400

    created: a function getSyncState()

--- sync_update.py
+++ sync_update.py
@@ -2,29 +2,52 @@ import numpy as np
 from PatternJitter import *
 from Data import *
 
-givenT = getOmega(5, x_tilde)
-print('Reference Train: ', Ref)
-print('Given T: ')
-# print(givenT)
-upState = []
-upStateIndex = []
-index = 0
+Ref = [8, 18, 23, 28, 34, 42, 44, 49]
 
-for j, Tj in enumerate(givenT):
+def getSyncState(L, Reference, Target):
+    ref = Reference
+    givenT = getOmega(L, Target)
+    print('Reference Train: ', ref)
+    # print('Given T: ')
 
-    print(j, Tj)
-    print('Reference[',j,']: ', Ref[j])
-    index = np.where(Tj == Ref[j])[0]
-    print('Index: ', index)
-    print('Index Size: ', index.size, '\n')
+    index = 0
+    upState = []
+    upStateIndex = []
+    syncStateMat = []
+    for j, Tj in enumerate(givenT):
+        # print('All possible T[',j,']: ', Tj)
+        # print('Reference[',j,']: ', ref[j])
+        index = np.where(Tj == ref[j])[0]
+        # print('Index: ', index)
+        # print('Index Size: ', index.size, '\n')
+        if index.size != 0:
+            # np.asscalar(a): Convert an array of size 1 to its scalar equivalent
+            index = np.asscalar(index)
+            # upStateIndex.append(index)
+            upState.append(Tj[index])
+        else:
+            # upStateIndex.append('No_Sync')
+            upState.append(0)
 
-    if index.size != 0:
-        upStateIndex.append(np.asscalar(index))
-    else:
-        upStateIndex.append(False)
+        n = len(Tj)
+        syncState = []
+        checkSync = upState[j]
+        # print('\n')
+        for i in range(n):
+            if Tj[i] == checkSync:
+                print('Check[', j,']: ', checkSync)
+                print('Tj[',i,']: ', Tj[i], 'is SYNCRONY.')
+                syncState.append(1)
+            else:
+                # print('Check[', j,']: ', checkSync)
+                # print('Tj:', Tj[i], 'is not Synch.')
+                syncState.append(0)
 
-    values = np.in1d(Tj, Ref)
-    # print('Where? : ', values, '\n')
+        syncStateMat.append(syncState)
 
+    syncStateMat = np.array(syncStateMat)
+    return syncStateMat
 
-print(upStateIndex)
+print(getSyncState(5, Ref, x_tilde))
+print('Transition Matrix: ')
+print(tDistMatrices)
