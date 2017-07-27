99d0c58259b281b2e9e7fe439226cae46d51d242
minwoobae <minubae.nyc@gmail.com>
2017-07-27 08:30:58 -0400

    modified: N-size

--- exp_sync_test.py
+++ exp_sync_test.py
@@ -7,7 +7,7 @@ from Data import *
 # x_tilde = [10, 15, 22, 29, 34, 40, 45, 51]
 # L = 5
 # R = 4
-
+Ref = [8, 13, 19, 28, 34, 42, 44, 49]
 def getSpikeTrainMat(L, R, obsX, N):
     spikeTrainMat = []
     for i in range(N):
@@ -18,20 +18,24 @@ def getSpikeTrainMat(L, R, obsX, N):
     Tmat = np.array(spikeTrainMat)
     return Tmat
 
-def getAmountSync(obsX, Tmat):
+def getAmountSync(Ref, Tmat):
     s = 0
     S = []
     for j, Tj in enumerate(Tmat):
         # Check how many elements are equal in two arrays (R, T)
         # print('Tj: ', Tj)
-        s = np.sum(obsX == np.array(Tj))
+        s = np.sum(Ref == np.array(Tj))
         S.append(s)
         # print('# Sync: ', s)
     return S
 
-Tmat = getSpikeTrainMat(5, 4, x_tilde, 1000)
+Tmat = getSpikeTrainMat(5, 4, x_tilde, 100)
+print('Spike Trains: ')
 print(Tmat)
-S = getAmountSync(x_tilde, Tmat)
+print('Reference Train: ')
+print(Ref)
+
+S = getAmountSync(Ref, Tmat)
 print('Amount_Synchrony: ', S)
 plt.hist(S, bins='auto')
 plt.show()
