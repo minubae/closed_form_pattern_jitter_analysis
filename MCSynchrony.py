9b86bf042bde480864b31ca30e54e94f925615ef
minwoobae <minubae.nyc@gmail.com>
2017-08-01 11:03:46 -0400

    MCSynchrony.py

--- MCSynchrony.py
+++ MCSynchrony.py
@@ -8,13 +8,31 @@ from Data import *
 # L = 5
 # R = 4
 Ref = [8, 13, 19, 28, 34, 42, 44, 49]
-Ref02 = [10, 14, 17]
+Ref_02 = [10, 14, 17]
+Ref_03 = [10, 14, 17, 20]
 
-def getSpikeTrainMat(L, R, obsX, N):
+def getSpikeTrainMat(L, R, obsX,initDist, tDistMatrices, N):
+
+    n = 0
+    length = 0
+    hisLen = 0
+
+    n = N
+    length = L
+    hisLen = R
+
+    initD = []
+    ObsTar = []
+    tDistMat = []
     spikeTrainMat = []
+
+    initD = initDist
+    tDistMat = tDistMatrices
+    ObsTar = obsX
+
     for i in range(N):
         print('[[[[[[[Spike Train Index: ', i,']]]]]]]')
-        surrogate = getSpikeTrain(obsX, L, R, initDist_02, tDistMatrices_02)
+        surrogate = getSpikeTrain(ObsTar, length, hisLen, initD, tDistMat)
         spikeTrainMat.append(surrogate)
 
     Tmat = np.array(spikeTrainMat)
@@ -25,23 +43,25 @@ def getAmountSync(Reference, Target):
     S = []
     Ref = []
     Tmat = []
-    Ref = Reference
+    ref = Reference
     Tmat = Target
     for j, Tj in enumerate(Tmat):
         # Check how many elements are equal in two arrays (R, T)
         # print('Tj: ', Tj)
-        s = np.sum(Ref == np.array(Tj))
+        s = np.sum(ref == np.array(Tj))
         S.append(s)
         # print('# Sync: ', s)
     return S
 
-Tmat = getSpikeTrainMat(3, 2, x_tilde_02, 10000)
+Tmat = getSpikeTrainMat(3, 2, x_tilde_03, initDist_03, tDistMatrices_03, 1000)
 print('Spike Trains: ')
 print(Tmat)
 print('Reference Train: ')
-print(Ref)
+print(Ref_03)
 
-S = getAmountSync(Ref02, Tmat)
+S_TrainN = len(x_tilde_03)
+S = getAmountSync(Ref_03, Tmat)
 print('Amount_Synchrony: ', S)
 plt.hist(S, bins='auto')
+# plt.axis([0, S_TrainN])
 plt.show()
